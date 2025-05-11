fdc9a6d2b74b8e392ba7880bd08356d3f5cefcc2
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-05-11 23:45:26 +0700

    terusan dari menambahkan rute

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,28 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  widget build(Buildcontext context) {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigasi App',
+      title: 'Navigasi dengan Tema',
       theme: ThemeData(
-        primarySwatch: colors.blue, 
-        
+        primarySwatch: Colors.blue, // Warna utama aplikasi
+        accentColor: Colors.amber, // Warna aksen untuk tombol, ikon, dll
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black87, fontSize: 18),
+          bodyText2: TextStyle(color: Colors.black54, fontSize: 16),
+          headline6: TextStyle(color: Colors.blueAccent, fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blue, // Warna tombol
+          textTheme: ButtonTextTheme.primary, // Warna teks tombol
+        ),
       ),
-    )
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/second': (context) => SecondPage(),
+        '/third': (context) => ThirdPage(), // Menambahkan rute untuk halaman ketiga
+      },
+    );
   }
 }
