bba96008a0e3aeb41bce44e420c054c84d0de6da
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-05-12 00:41:49 +0700

    perubahan pada code yang tidak sesuai

--- lib/main.dart
+++ lib/main.dart
@@ -10,32 +10,31 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-  title: 'Navigasi dengan Tema',
-  theme: ThemeData(
-    colorScheme: ColorScheme.fromSwatch(
-      primarySwatch: Colors.blue,
-    ).copyWith(
-      secondary: Colors.amber, // pengganti accentColor
-    ),
-    textTheme: TextTheme(
-      bodyLarge: TextStyle(color: Colors.black87, fontSize: 18), // pengganti bodyText1
-      bodyMedium: TextStyle(color: Colors.black54, fontSize: 16), // pengganti bodyText2
-      titleLarge: TextStyle(color: Colors.blueAccent, fontSize: 22, fontWeight: FontWeight.bold), // pengganti headline6
-    ),
-  ),
-);
-
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.blue, // Warna tombol
-          textTheme: ButtonTextTheme.primary, // Warna teks tombol
+    return MaterialApp(
+      title: 'Navigasi dengan Tema',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blue,
+        ).copyWith(
+          secondary: Colors.amber, // pengganti accentColor
+        ),
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.black87, fontSize: 18), // pengganti bodyText1
+          bodyMedium: TextStyle(color: Colors.black54, fontSize: 16), // pengganti bodyText2
+          titleLarge: TextStyle(color: Colors.blueAccent, fontSize: 22, fontWeight: FontWeight.bold), // pengganti headline6
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue, // Warna tombol
+            foregroundColor: Colors.white, // Warna teks
+          ),
         ),
       ),
       initialRoute: '/',
       routes: {
         '/': (context) => HomePage(),
         '/second': (context) => SecondPage(),
-        '/third': (context) => ThirdPage(), // Menambahkan rute untuk halaman ketiga
+        '/third': (context) => ThirdPage(),
       },
     );
   }
