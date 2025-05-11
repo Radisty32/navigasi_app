f1d2437fc79c3a5c61d5e76e3da9507af7fafd4c
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-05-11 23:56:10 +0700

    menambahkan item navigasi untuk menuju halaman ketiga.

---
+++ lib/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    if (index == 1) {
+      Navigator.pushNamed(context, '/second');
+    } else if (index == 2) {
+      Navigator.pushNamed(context, '/third'); // Navigasi ke halaman ketiga
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page', style: Theme.of(context).textTheme.headline6),
+      ),
+      body: Center(
+        child: Text(
+          'Halaman Utama',
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.arrow_forward),
+            label: 'Second Page',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pageview),
+            label: 'Third Page', // Menambahkan item untuk halaman ketiga
+          ),
+        ],
+      ),
+    );
+  }
+}
